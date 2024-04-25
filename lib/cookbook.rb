221fdd80202b6edfcccd5a1cbd655ba518e38790
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-04-25 21:19:24 +0900

    done with the model and repo

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -1,1 +1,18 @@
-# TODO: Implement the Cookbook class that will be our repository
+class Cookbook
+  def initialize
+    @recipes = [] # We are going to store many different Recipe instances
+  end
+
+  def all
+    # Return all the recipes in the cookbook
+    return @recipes
+  end
+
+  def create(recipe) # Takes Recipe instance as an argument
+    @recipes << recipe
+  end
+
+  def destroy(recipe_index) # Expect an integer as an argument
+    @recipes.delete_at(recipe_index)
+  end
+end
